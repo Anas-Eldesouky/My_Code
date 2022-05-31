a0145488728c8b390455213c6598f543c2367a6c
Anas Eldesouky <877148@pdsb.net>
2022-05-31 00:30:17 -0400

    color changes

--- game.py
+++ game.py
@@ -4,19 +4,20 @@ pygame.init()
 
 menu = pygame.display.set_mode([1920, 1080])
 menu.fill((65,90,120))
-
+white = (255, 255, 255)
+black = (0, 0, 0)
 
 x = pygame.Rect(910, 515, 100, 50)
 x2 = pygame.Rect(910, 575, 100, 50)
-start_button = pygame.draw.rect(menu, (0, 0, 0), x)
-quit_button = pygame.draw.rect(menu, (0, 0, 0), x2)
-start_button_border = pygame.draw.rect(menu, (255, 255, 255), x, 2)
-quit_button_border = pygame.draw.rect(menu, (255, 255, 255), x2, 2)
+start_button = pygame.draw.rect(menu, black, x)
+quit_button = pygame.draw.rect(menu, black, x2)
+start_button_border = pygame.draw.rect(menu, white, x, 2)
+quit_button_border = pygame.draw.rect(menu, white, x2, 2)
 font = pygame.font.SysFont("Corbel", 19)
 title_font = pygame.font.SysFont("Ariel", 100)
-text = font.render("Start" , True, [255, 255, 255])
-text2 = font.render("Quit", True, [255, 255, 255])
-title_text = title_font.render("Flappy Bird", True, [255, 255, 255])
+text = font.render("Start" , True, white)
+text2 = font.render("Quit", True, white)
+title_text = title_font.render("Flappy Bird", True, white)
 text2_rect = text2.get_rect(center = (1920 / 2, (1080 / 2) + 60))
 text_rect = text.get_rect(center = (1920 / 2, 1080 / 2))
 title_rect = title_text.get_rect(center = (1920 / 2, (1080 / 2) - 150))
@@ -25,7 +26,7 @@ menu.blit(text2, text2_rect)
 menu.blit(title_text, title_rect)
 
 def start():
-	menu.fill([0, 0, 0])
+	menu.fill(black)
 
 var = True
 while var:
